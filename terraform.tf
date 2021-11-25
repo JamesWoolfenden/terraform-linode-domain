6a25f5ceaa940ca2122f44d0dee32147d8a18ebc
James Woolfenden <James.Woolfenden@gmail.com>
2021-11-25 09:24:46 +0000

    draft

--- terraform.tf
+++ terraform.tf
@@ -4,18 +4,6 @@ terraform {
       source  = "linode/linode"
       version = ">=1.25.0"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.0.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "3.0.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
   }
   required_version = ">=0.14.8"
 }
